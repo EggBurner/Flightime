1289e48b061712913e933b7ebda2d97e74bdd436
Abdullah Islam <itsabddev@gmail.com>
2026-04-05 23:24:06 +0500

    did a lot

---
+++ app/flight/page.tsx
@@ -0,0 +1,9 @@
+import FlightMapLoader from '../components/FlightMapLoader';
+
+export default function FlightPage() {
+  return (
+    <main className="flex-1 relative">
+      <FlightMapLoader />
+    </main>
+  );
+}
